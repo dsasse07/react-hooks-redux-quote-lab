da22215f78486e266eed4520dd8ddc6bec6992ef
Daniel Sasse <dsasse07@gmail.com>
2021-03-13 13:40:22 -0500

    commit tree e0c64a0385433a79c2d0a65339336da99294e12d

--- src/features/quotes/QuoteCard.js
+++ src/features/quotes/QuoteCard.js
@@ -1,15 +1,34 @@
 import React from "react";
+import { removeQuote, upvoteQuote, downvoteQuote } from "./quotesSlice";
+import { useDispatch } from 'react-redux'
 
-function QuoteCard(props) {
+function QuoteCard({quote}) {
+  const {id, content, author, votes} = quote
+  const dispatch = useDispatch()
+
+  function handleClick(event){
+    switch (true){
+      case (event.target.name === "remove"):
+        dispatch( removeQuote(id) )
+        break
+      case (event.target.name === "upvote"):
+        dispatch( upvoteQuote(id) )
+        break
+      case (event.target.name === "downvote"):
+        dispatch( downvoteQuote(id) )
+        break
+    }
+  }
+  
   return (
     <div>
       <div className="card card-inverse card-success card-primary mb-3 text-center">
         <div className="card-block">
           <blockquote className="card-blockquote">
-            <p>{/*Render Quote Content*/}</p>
+            <p>{content}</p>
             <footer>
               - author{" "}
-              <cite title="Source Title">{/*Render Quote Author*/}</cite>
+              <cite title="Source Title">{author}</cite>
             </footer>
           </blockquote>
         </div>
@@ -19,17 +38,17 @@ function QuoteCard(props) {
             role="group"
             aria-label="Basic example"
           >
-            <button type="button" className="btn btn-primary">
+            <button type="button" className="btn btn-primary" name="upvote" onClick={handleClick}>
               Upvote
             </button>
-            <button type="button" className="btn btn-secondary">
+            <button type="button" className="btn btn-secondary" name="downvote" onClick={handleClick}>
               Downvote
             </button>
-            <button type="button" className="btn btn-danger">
+            <button type="button" className="btn btn-danger" name="remove" onClick={handleClick}>
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div>Votes: {/*Render Quote Votes*/}</div>
+          <div>Votes: {votes}</div>
         </div>
       </div>
     </div>
